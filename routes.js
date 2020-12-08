732dd5e0eeda87e5a5bdabee550fad81a07adb87
tony <tonyfrenchdm@gmail.com>
2020-12-08 11:53:59 -0500

    finished post endpoint

--- routes.js
+++ routes.js
@@ -10,4 +10,23 @@ routes.get("/places", (req, res) => {
   });
 });
 
+routes.post("/places", (req, res) => {
+  let query = `INSERT INTO pride (name, address, phonenumber, lat, long, safe, info)
+  VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+  pool
+    .query(query, [
+      req.body.name,
+      req.body.address,
+      req.body.phonenumber,
+      req.body.lat,
+      req.body.long,
+      req.body.safe,
+      req.body.info,
+    ])
+    .then((response) => {
+      res.status(201);
+      res.json(req.body);
+    });
+});
+
 module.exports = routes;
