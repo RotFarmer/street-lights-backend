58581c12fa10d382cf3a47f96dbda58b1f02e089
tony <tonyfrenchdm@gmail.com>
2020-12-15 16:03:29 -0500

    fixed my mistakes

--- routes.js
+++ routes.js
@@ -4,15 +4,15 @@ const routes = express.Router();
 const pool = require("./connection");
 
 routes.get("/places", (req, res) => {
-  let query = `SELECT * FROM pride`;
+  let query = `SELECT * FROM places`;
   pool.query(query).then((response) => {
     res.json(response.rows);
   });
 });
 
 routes.post("/places", (req, res) => {
-  let query = `INSERT INTO pride (name, address, phonenumber, lat, long, safe, info)
-  VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+  let query = `INSERT INTO places (name, address, phonenumber, lat, long, safe)
+  VALUES ($1, $2, $3, $4, $5, $6)`;
   pool
     .query(query, [
       req.body.name,
@@ -21,8 +21,25 @@ routes.post("/places", (req, res) => {
       req.body.lat,
       req.body.long,
       req.body.safe,
-      req.body.info,
     ])
+    .then((response) => {
+      res.status(201);
+      res.json(req.body);
+    });
+});
+
+routes.get("/reports", (req, res) => {
+  let query = `select * from reports where place_id = ${req.query.id}`;
+  pool.query(query).then((response) => {
+    res.json(response.rows);
+  });
+});
+
+routes.post("/reports", (req, res) => {
+  let query = `INSERT INTO reports (place_id, report, date)
+  VALUES ($1, $2, $3)`;
+  pool
+    .query(query, [req.body.place_id, req.body.report, req.body.date])
     .then((response) => {
       res.status(201);
       res.json(req.body);
